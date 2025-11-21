3d97494c0cf5d002d79cedc59bb2d9cdb4843d54
sriram <ss2d22@soton.ac.uk>
2025-11-21 02:23:40 +0000

    changex z axis for sheet content for mobile

--- southampton_crime_map_frontend/components/navbar.tsx
+++ southampton_crime_map_frontend/components/navbar.tsx
@@ -122,7 +122,7 @@ export function Navbar() {
                   )}
                 </Button>
               </SheetTrigger>
-              <SheetContent side="right" className="w-[85vw] max-w-sm p-0">
+              <SheetContent side="right" className="w-[85vw] max-w-sm p-0 z-[9999]">
                 <div className="flex flex-col h-full">
                   {/* Header */}
                   <div className="p-6 border-b border-border/50">
